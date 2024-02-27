007b0cce8cf6cc402b2f4d1c73565842f49c91b0
Fabrusio <fabrusio3p3@gmail.com>
2024-02-26 23:49:26 -0300

    MVC del apartado productos modificado para añadir el tipo correspondiente

--- html/controllers/crud_products.php
+++ html/controllers/crud_products.php
@@ -84,4 +84,61 @@ class Crud_products extends SessionController {
         }
     }
 
+    // TIPOS DE UNIDAD/PESAJE
+
+    public function createType(){
+        if($this->existPOST(['nameType'])){
+
+            $nameType = strtolower($_POST["nameType"]);
+            $productsModel = new ProductsModel();
+            if ($productsModel->nameTypeExists(0, $nameType)) {
+                echo "El nombre del tipo ya está en uso. Por favor, elige otro.";
+                error_log("El nombre del tipo ya esta en uso");
+            } else {
+
+                if ($productsModel->createType($nameType)) {
+                    error_log("Tipo se esta enviado ------------creando-----------.");
+                } else {
+                    error_log("Error al intentar enviar la categoria.");
+                }
+            }
+        }
+    }
+
+    public function deleteType(){
+        if($this->existPOST(['id'])){
+            $id = $this->getPOST('id');
+
+
+            $productsModel = new ProductsModel();
+        if ($productsModel->deleteType($id)) {
+            error_log("Enviando tipo a eliminar -----eliminando------ " . $id);
+        } else {
+            error_log("Error al eliminar la categoria con ID " . $id);
+        }
+            
+        }
+    }
+
+    public function editType(){
+        if($this->existPOST(['id','name'])){
+            $id = $_POST["id"];
+            $name = strtolower($_POST["name"]);
+            // Verificar si el nuevo nombre de tipo ya existe en la base de datos
+            $productsModel = new ProductsModel();
+            if ($productsModel->nameTypeExists($id, $name)) {
+                echo "El nombre del tipo ya está en uso. Por favor, elige otro.";
+                error_log("El nombre ya esta en uso");
+            } else {
+
+                $productsModel = new ProductsModel();
+                if ($productsModel->updateType($id, $name)) {
+                    echo "tipo actualizando.";
+                } else {
+                    echo "Error al actualizar el tipo.";
+                }
+            }
+        }
+    }
+
 }//Cierra clase
