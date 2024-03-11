3ab032e184075945d44d6af93d3ecf63ac9de840
Fabrusio <fabrusio3p3@gmail.com>
2024-03-10 22:47:04 -0300

    phpmailer configurado y recibimiento de datos exitoso + verificación

--- html/views/sales/enviar_correo.php
+++ html/views/sales/enviar_correo.php
@@ -7,44 +7,62 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
 require 'PHPMailer-master/src/Exception.php';
 require 'PHPMailer-master/src/PHPMailer.php';
 require 'PHPMailer-master/src/SMTP.php';
 
-error_log("LLEGO A ENTRAR A ENVIAR CORREO, VEMOS SI ANDA O NO ");
+function validarEmail($email) {
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      return false;
+  }
+  // Verificar si el dominio del correo electrónico tiene registros MX
+  list($username, $domain) = explode('@', $email);
+  return checkdnsrr($domain, 'MX');
+}
 
-// Obtener los datos enviados a través de la solicitud POST
-$rutaPDF = $_POST['rutaPDF'];
-$correoDestinatario = isset($_POST['correoDestinatario']) ? $_POST['correoDestinatario'] : "";
-error_log($correoDestinatario);
+if (isset($_POST['email']) && isset($_FILES['pdf'])) {
+  $emailCliente = trim($_POST['email']);
 
-$mail = new PHPMailer();
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPAuth = true;
-$mail->Username = 'appfoodsoporte@gmail.com';
-$mail->Password = 'fabriziolucasgonzalo';
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
+  if (!validarEmail($emailCliente)) {
+    echo json_encode(['success' => false, 'message' => 'El correo electrónico ingresado no es válido.']);
+    exit; // Salir del script si el correo electrónico no es válido
+  }
 
-// Configurar el remitente
-$mail->setFrom('appfoodsoporte@gmail.com', 'AppFoodSoporte');
 
-// Agregar el destinatario
-$mail->addAddress($correoDestinatario);
+  $archivoAdjunto = $_FILES['pdf']['tmp_name']; 
+  $nombreArchivo = $_FILES['pdf']['name'];
 
-// Configurar el asunto y el cuerpo del mensaje
-$mail->Subject = 'Comprobante de venta';
-$mail->Body = 'Hola, aquí te enviamos el comprobante de tu compra en formato PDF.';
+  $mail = new PHPMailer(true);
 
-// Adjuntar el archivo PDF
-//$mail->addAttachment($rutaPDF); // Adjuntamos el archivo PDF generado
+      $mail->SMTPDebug = 0;                      // Enable verbose debug output (remove for production)
+      $mail->isSMTP();                                            // Send using SMTP
+      $mail->Host       = 'smtp.gmail.com';                     // Set the SMTP server to send through
+      $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+      $mail->Username   = 'appfoodsoporte@gmail.com';                     // SMTP username
+      $mail->Password   = 'namtybaapzckjeem';                               // SMTP password
+      $mail->SMTPSecure = 'tls';            // Enable implicit TLS encryption
+      $mail->Port       = 587;                                    // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-// Enviar el correo electrónico
-if (!$mail->send()) {
-  echo 'enviar_correo.php===> Error al enviar el correo electrónico: ' . $mail->ErrorInfo;
+      // Recipients
+      $mail->setFrom('appfoodsoporte@gmail.com', 'AppFoodSoporte');
+      $mail->addAddress($emailCliente);     // Add a recipient
+
+      // Attachments
+      $mail->addAttachment($archivoAdjunto, $nombreArchivo);         // Add the uploaded PDF
+
+      // Content
+      $mail->isHTML(true);
+      $mail->Subject = 'Comprobante de venta';
+      $mail->Body    = 'Hola, aquí te enviamos el comprobante de tu compra en formato PDF.';
+
+      try {
+        $mail->send();
+        echo json_encode(['success' => true, 'message' => 'Correo electrónico enviado correctamente.']);
+    } catch (Exception $e) {
+        echo json_encode(['success' => false, 'message' => 'Hubo un error al enviar el correo electrónico: ' . $mail->ErrorInfo]);
+    }
 } else {
-  echo 'enviar_correo.php===> Correo electrónico enviado correctamente.';
+    // Enviar mensaje de error si faltan datos
+    echo json_encode(['success' => false, 'message' => 'Faltan datos en la solicitud']);
 }
 ?>
